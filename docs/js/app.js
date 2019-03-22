209eabe91bcdb74c07216f25ee3b1f68652df629
geprojetos <geprojetosbrasil@gmail.com>
2019-03-22 13:57:37 -0300

    Buscando por um repositório hard coded

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,47 +1,35 @@
 (function(){
     "use-strict"
 
-    let listRepos   = [
-        {
-            title: 'Primeiro',
-            img: 'https://trello-avatars.s3.amazonaws.com/0c984c7d40ee3680fa8e1eb97a21bd69/30.png',
-            desc: 'DEscrição 1',
-            rep: 'Link do repositório 1'
-        },
-        {
-            title: 'Segundo',
-            img: 'https://yt3.ggpht.com/a-/AAuE7mDMgJdxLr67xIch3lj0egc9RZXiZhMXIglFew=s48-mo-c-c0xffffffff-rj-k-no',
-            desc: 'DEscrição 2',
-            rep: 'Link do repositório 2'
-        }
-    ];
-    let repsList        = document.querySelector('.reps');
+    let baseUrl     = 'https://api.github.com/repos';
+    let listRepos   = [];
+    let wrapperList = document.querySelector('.wrapper-list');
 
     
     function _render() {
 
         console.log('Exibe os repositórios');
+        console.log(listRepos);
+        
         
         _createItem();
     };
     
     function _createItem() {
 
-        // reps.innerHTML = '';
-
         for(list of listRepos) {
             
-            repsList.innerHTML += `
+            wrapperList.innerHTML += `
                 <li class="col-6">
                     <div class="card mt-2">
                         <div class="card-header bg-dark text-white">
-                            <h3 class="card-title">${ list.title }</h3>
+                            <h3 class="card-title">${ list.name }</h3>
                         </div>
                         <div class="card-body">
-                            <img class="img-fluid card-img-top" src="${ list.img }" alt="${ list.desc }">
-                            <p class="card-text">${ list.desc }</p>
+                            <img class="img-fluid card-img-top" src="${ list.owner.avatar_url }" alt="${ list.description }">
+                            <p class="card-text">${ list.description }</p>
                             <div>
-                                <a href="${ list.rep }" class="btn btn-info btn-block mt-2 clear-both">Acessar repositório</a>
+                                <a href="${ list.url }" class="btn btn-info btn-block mt-2 clear-both">Acessar repositório</a>
                                 <a href="#" class="btn btn-danger btn-block mt-2">Remover</a>
                             </div>
                         </div>
@@ -50,8 +38,37 @@
             `;
         }
 
+    };
+
+    function _findRepository(repository) {
+
+        return fetch(`${ baseUrl }/${ repository }`)
+            .then(res => {
+
+                if(!res.ok) {
+                    throw res;
+                } else {
+                    return res.json();
+                }
+            })
+            .then(rep => _addRepository(rep))
+            .catch(erro => {
+                console.log(erro);
+                return erro;
+            })
+    };
+
+    function _addRepository(rep) {
+        
+        listRepos.push(rep);
+        _render();
     }
 
+    function _handleSubmit() {
+
+        _findRepository('geprojetos/rede-social-angular');
+    };
+
     _render();
-    
+    _handleSubmit();
 })()
